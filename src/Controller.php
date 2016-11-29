4533b84f6b3248d6b85e2d24bf1b922ce2bd01be
Roman Zimmermann <roman@more-onion.com>
2016-11-29 15:47:30 +0100

    Use new payment_forms API.

--- src/Controller.php
+++ src/Controller.php
@@ -12,10 +12,17 @@ class Controller extends \PaymentMethodController {
 
   public function __construct() {
     $this->title = t('Sage Pay');
-    $this->form = new RedirectForm();
 
-    $this->payment_configuration_form_elements_callback = 'payment_forms_method_form';
-    $this->payment_method_configuration_form_elements_callback = '\Drupal\sagepay_payment\configuration_form';
+    $this->payment_configuration_form_elements_callback = 'payment_forms_payment_form';
+    $this->payment_method_configuration_form_elements_callback = 'payment_forms_method_configuration_form';
+  }
+
+  public function paymentForm() {
+    return new RedirectForm();
+  }
+
+  public function configurationForm() {
+    return new ControllerForm();
   }
 
   public function execute(\Payment $payment) {
@@ -153,115 +160,5 @@ class Controller extends \PaymentMethodController {
     db_delete('sagepay_payment_payment_method_controller')
       ->condition('pmid', $method->pmid)
       ->execute();
-  }
-}
-
-/* Implements PaymentMethodController::payment_method_configuration_form_elements_callback().
- *
- * @return array
- *   A Drupal form.
- */
-function configuration_form(array $form, array &$form_state) {
-  $method = $form_state['payment_method'];
-  $controller_data = $method->controller_data + $method->controller->controller_data_defaults;
-
-  $form['#tree'] = TRUE;
-  $form['testmode'] = array(
-    '#type' => 'checkbox',
-    '#title' => t('test mode'),
-    '#default_value' => isset($controller_data['testmode']) ? $controller_data['testmode'] : '',
-  );
-
-  $form['vendorname'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Vendor name'),
-    '#required' => true,
-    '#default_value' => isset($controller_data['vendorname']) ? $controller_data['vendorname'] : '',
-  );
-
-  $form['partnerid'] = array(
-    '#type' => 'textfield',
-    '#title' => t('Partner ID'),
-    '#required' => false,
-    '#default_value' => isset($controller_data['partnerid']) ? $controller_data['partnerid'] : '',
-  );
-
-  $form['personal_data'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Personal data'),
-    '#description' => t('Configure how personal data can be mapped from the payment context.'),
-  );
-
-  $display_options = array(
-    'ifnotset' => t('Show field if it is not available from the context.'),
-    'always' => t('Always show the field - prefill with context values.'),
-  );
-  $non_mandatory = array(
-    'hidden' => t("Don't display, use values from context if available."),
-  );
-  $extra = RedirectForm::extraElements();
-
-  foreach ($extra as $key => &$element) {
-    $element['#top_level'] = TRUE;
-  }
-
-  $stored = &$controller_data['personal_data'];
-  $flat = RedirectForm::flatten($extra);
-  foreach ($flat as $key => &$element) {
-    $defaults = isset($stored[$key]) ? $stored[$key] : array();
-    $defaults += array('display' => 'ifnotset', 'keys' => array($key), 'mandatory' => FALSE);
-    $form['personal_data'][$key] = array(
-      '#type' => 'fieldset',
-      '#title' => $element['#title'],
-    );
-    $required = !empty($element['#required']);
-    $defaults['mandatory'] = $defaults['mandatory'] || $required;
-    $id = drupal_html_id('controller_data_' . $key);
-    if (!empty($element['#top_level'])) {
-      $form['personal_data'][$key]['display'] = array(
-        '#type' => 'select',
-        '#title' => t('Display'),
-        '#options' => ($required ? array() : $non_mandatory) + $display_options,
-        '#default_value' => $defaults['display'],
-        '#id' => $id,
-      );
-    }
-    $form['personal_data'][$key]['mandatory'] = array(
-      '#type' => 'checkbox',
-      '#title' => t('Mandatory'),
-      '#states' => array(
-        'disabled' => array(
-          "#$id" => array('value' => 'hidden'),
-        ),
-      ),
-      '#default_value' => $defaults['mandatory'],
-      '#access' => !$required,
-    );
-    if ($element['#type'] != 'fieldset') {
-      $form['personal_data'][$key]['keys'] = array(
-        '#type' => 'textfield',
-        '#title' => t('Context keys'),
-        '#description' => t('When building the form these (comma separated) keys are used to ask the Payment Context for a (default) value for this field.'),
-        '#default_value' => implode(', ', $defaults['keys']),
-      );
-    }
-  }
-  return $form;
-}
-
-/**
- * Implements form validate callback for
- * \stripe_payment\configuration_form().
- */
-function configuration_form_validate(array $element, array &$form_state) {
-  $values = drupal_array_get_nested_value($form_state['values'], $element['#parents']);
-  $data = &$form_state['payment_method']->controller_data;
-  $data['testmode'] = $values['testmode'];
-  $data['partnerid'] = $values['partnerid'];
-  $data['vendorname'] = $values['vendorname'];
-  $data['personal_data'] = $values['personal_data'];
-  foreach ($data['personal_data'] as &$field) {
-    $field += array('keys' => '');
-    $field['keys'] = array_map('trim', explode(',', $field['keys']));
   }
 }
